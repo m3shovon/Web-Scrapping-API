9d376fe6eff90703fb2b955f179add553e4f35a7
Mufrid Shovon <mia.md.mufrid@gmail.com>
2024-12-07 17:48:16 +0600

    filter admin

--- App_Scrapping/admin.py
+++ App_Scrapping/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import ScrapedData
 
-admin.site.register(ScrapedData)
+class ScrapedDatatable(admin.ModelAdmin):
+    list_display = ['id','title','url','scraped_at']
+    search_fields = ( 'url',)
+
+admin.site.register(ScrapedData,ScrapedDatatable)
